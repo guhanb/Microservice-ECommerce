ad8c473f5f8b7a548606a22583f509ec66be2e8d
guhanb <guhanreo@gmail.com>
2026-06-23 06:51:36 +0530

    Ecommerce Rest APIs

---
+++ ecommerce/src/main/java/com/ecom/ecommerce/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.ecom.ecommerce.model;
+
+public enum UserRole {
+    CUSTOMER, ADMIN
+}
---
+++ ecommerce/src/main/java/com/ecom/ecommerce/repository/CartItemRepository.java
@@ -0,0 +1,21 @@
+package com.ecom.ecommerce.repository;
+
+
+import com.ecom.ecommerce.model.CartItem;
+import com.ecom.ecommerce.model.Product;
+import com.ecom.ecommerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    CartItem findByUserAndProduct(User user, Product product);
+
+    void deleteByUserAndProduct(User user, Product product);
+
+    List<CartItem> findByUser(User user);
+
+    void deleteByUser(User user);
+}
